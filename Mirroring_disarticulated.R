04c52c13cdd634cc9fc7cf9981f378133624fe6f
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2022-05-24 22:46:06 +0100

    Update Mirroring_disarticulated.R

--- Mirroring_disarticulated.R
+++ Mirroring_disarticulated.R
@@ -1,10 +1,7 @@
 
-####################################################################
-#                                                                  #
-#     MIRROR YOUR SLID, RESAMPLED LMs                              #
-#     NOTE: DIFFERENT CODE FOR SYMMETRIC AND ASYMMETRIC SPECIMENS  #
-#                                                                  #
-####################################################################
+#========================================#
+#      1. READ IN THE MANUAL LMS         #
+#========================================#
 
 
 library(Rvcg)
@@ -14,19 +11,14 @@ library(rgl)
 library(geomorph)
 library(paleomorph)
 
-#========================================#
-#      1. READ IN THE MANUAL LMS         #
-#========================================#
-
-
 ###=========== LOADING DATA SET 1: WHOLE LANDMARKED SKULL 
 #Read in landmarks manually placed on the whole skull 
 
-ntaxa<-172 ## number of specimens (extant only) - NB can also put this in the code below (x,y,z) 
+ntaxa<-4 ## number of specimens (extant only) - NB can also put this in the code below (x,y,z) 
 #data set .pts from Checkpoint
 
 ptslist<-dir(pattern='.pts',recursive=T)
-ptsarray<-array(dim=c(123,3,172)) #dim=c(number of landmarks and semilandmarks, number of dimensions, number of specimens)
+ptsarray<-array(dim=c(36,3,4)) #dim=c(number of landmarks and semilandmarks, number of dimensions, number of specimens)
 for(i in 1:length(ptslist))
 {
   ptsarray[,,i]<-as.matrix(read.table(file=ptslist[i],skip=2,header=F,sep="",row.names=1))
@@ -39,155 +31,78 @@ dimnames(ptsarray)[3]<-list(
 arraylm<-ptsarray #this is your array
 
 
+##### MISSING LANDMARKS #########
+#do this first and then rearrange landmarks 
+#Landmarks need to be 'reshuffled' because we added 4 extra LMs on the nasal to be reflect that one midline landmark would 
+#not work in the asymmetrical odontocetes 
+
+arraylm[which(arraylm==9999)] <- NA
+arraylm <- estimate.missing(arraylm,method="TPS")
+
+
+#Check the LMs
+text3d(arraylm[,,1], text = 1:36)
+
 #Read in RHS and make a fake midline for mysticetes and any disarticulated archs 
 
-LM1_bilat=slidedlmsZARHI[c(5,69),,] ## LM1 on left (5) and corresponding LM on the right side (69)
-LM2_bilat=slidedlmsZARHI[c(6,70),,] ## LM2 on left (6) and corresponding LM on the right side (70)
+LM1_bilat=arraylm[c(9,33),,] ## LM1 on left (5) and corresponding LM on the right side (69)
+LM2_bilat=arraylm[c(10,34),,] ## LM2 on left (6) and corresponding LM on the right side (70)
+LM3_bilat=arraylm[c(12,35),,] ## LM1 on left (5) and corresponding LM on the right side (69)
+LM4_bilat=arraylm[c(13,36),,] 
 
-#Check the dimensions 
+#Check the dimensions  
 #now want to find the midpoint of these pairs, 
 #which is like finding the mean of each pair's position. 
 #So to do this I summed each specimens x, then y then z coords, 
 LM1_midline=colSums(LM1_bilat)/2
 LM2_midline=colSums(LM2_bilat)/2
-#LM3_midline=colSums(LM3_bilat)/2
-
-
-#Now you've created your 4 new mildine points, these should become 9, 10, 12, and 13 to correspond with odonts 
-
-
-
-
-
-
-
-
-
-
-
-
-#called 'slidedlm_fake' because this data set still contains the additional midline LMs to help with alignment 
-#removing the fake midline landmarks is carried out after mirroring (see below)
-
-slidedlms_fake <- Shape_data_with_bilats
-#slidedlms_fake[c(80:82,87:119),,]<-NA 
-
-open3d();spheres3d(slidedlms_fake[,,1])
-left.curves<-c(1:64)
-left.lm <- c(1:37,39,41:47,50,52,53,57:60,62:66)
-right.lm <- c(120, 121, 67:82, 122, 123, 83:119)
-
-right.curves <- c(65:85)
-left.curve.list<-unlist(my_curves$Curve.in[left.curves])
-right.curve.list<-unlist(my_curves$Curve.in[right.curves])
-leftside<-c(left.lm,left.curve.list) # RHS LMs+ RHS curves+all patch points
-rightside<-c(right.lm, right.curve.list)
-num.missing<-(length(leftside)-length(rightside)) # number of LMs to create= total RHS-current LHS LMs
-blanks<-c((dim(slidedlms_fake)[1]+1):(dim(slidedlms_fake)[1]+num.missing))
-# to fill in blanks from one row past the last current point, for the number of rows needed (num.missing)
-rightside<-c(rightside,blanks)
-add_col_or_row = function(x, n = 1, add_col = T, fill = 0)
-{
-  m1 = matrix(x, ncol = if(add_col) nrow(x) * ncol(x) else nrow(x), byrow = T)
-  m2 = matrix(fill, nrow = if(add_col) dim(x)[3] else prod(dim(x)[-1]),
-              ncol = if(add_col) nrow(x) * n else n)
-  array(t(cbind(m1, m2)),
-        c(nrow(x) + ((!add_col) * n), ncol(x) + (add_col * n), dim(x)[3]))
-}
-specimens2<-add_col_or_row(slidedlms_fake,n=num.missing,add_col=FALSE,fill=NA)
-specimens2[c(80:82,87:119),,]<-NA
-dimnames(specimens2)[3]<-dimnames(slidedlms_fake)[3]
-#bilats <- read.csv('bilats.csv')
-bilats<-cbind(leftside,rightside)
-newarray<-mirrorfill(specimens2,l1=midline,l2=bilats)
-dimnames(newarray)[3]<-dimnames(slidedlms)[3]
-open3d();
-spheres3d(newarray[,,3],radius=1.5)
-spheres3d(newarray[bilats[,1],,1],col='red',radius=1.5)
-spheres3d(newarray[bilats[,2],,1],col='blue',radius=1.5)
-spheres3d(newarray[midline,,1], col = 'yellow', radius = 1.5)
-
-
-midline<-as.integer(c(38,40,48,49,51,54,55,56,61,1449,1450)) 
-
-spheres3d(final_mirrored_odonts[c(1:37,39,41:47,50,52,53,57:60, 62:66),,1],col='red',radius=3) #LHS landmarks - MIDLINE
-spheres3d(final_mirrored_odonts[c(67:79,83:86,120:123),,1],col='red',radius=3) #RHS landmarks manually placed 
-spheres3d(final_mirrored_odonts[c(80:82,87:119),,1],col='green',radius=3) #RHS landmarks mirrored 
-spheres3d(final_mirrored_odonts[c(midline),,1],col='black',radius=3)
-#spheres3d(newarray[c(116:119),,1],col='magenta',radius=1.5)
-spheres3d(final_mirrored_odonts[c(124:1113),,1],col='green',radius=3) #LHS curves
-spheres3d(final_mirrored_odonts[c(1114:1448),,1],col='brown',radius=3) #RHS curves manual 
-spheres3d(final_mirrored_odonts[c(1449:1450),,1],col='yellow',radius=3) #fake midline LMs
-spheres3d(final_mirrored_odonts[c(1451:2105),,1],col='green',radius=3) #RHS mirrored 
-
-
-#Remove the extra fake landmarks first - numbers are different because of the extra curves on odonts 
-#For odonts and archs 
-final_mirrored_odonts=newarray[-c(1449:1450),,] 
-
-#landmarks will now change 
-spheres3d(final_mirrored_odonts[c(1:37,39,41:47,50,52,53,57:60, 62:66),,1],col='red',radius=1.5) #LHS 
-spheres3d(final_mirrored_odonts[c(midline),,1],col='black',radius=1.5)
-spheres3d(final_mirrored_odonts[c(67:123),,1],col='blue',radius=1.5) #RHS LMS
-spheres3d(final_mirrored_odonts[c(124:1113),,1],col='green',radius=1.5) #LHS curves 
-spheres3d(final_mirrored_odonts[c(1114:2103),,1],col='yellow',radius=1.5) #RHS curves
-
-#Remove the double midline: curves 35, 45, 
-final_mirrored_odonts=final_mirrored_odonts[-c(1629:1648, 1764:1783, 1824:1838, 1884:1903),,] #remove double midline 
-
-#Final dataset 
-spheres3d(final_mirrored_odonts[c(1:66),,2],col='red',radius=4)
-spheres3d(final_mirrored_odonts[c(67:123),,2],col='red',radius=4)
-spheres3d(final_mirrored_odonts[c(124:458),,2],col='blue',radius=4)
-spheres3d(final_mirrored_odonts[c(459:1113),,2],col='yellow',radius=4)
-spheres3d(final_mirrored_odonts[c(1114:1448),,2],col='blue',radius=4)
-spheres3d(final_mirrored_odonts[c(1449:2028),,2],col='yellow',radius=4)
-
-###################################################
-#                                                 #
-#  Make the same as mysticetes - correct order    #
-#                                                 #
-###################################################
-
-#Slot in the landmarks 
-#You can use abind to bind datasets - this rearranges the data into the same order as the mysts (i.e. without the weird 1-120, 2 = 121, 19 - 122, 20 -123)
-arranged_odonts=abind::abind(final_mirrored_odonts[c(1:66),,], final_mirrored_odonts[120,,],
-                             final_mirrored_odonts[c(67:68),,],
-                             final_mirrored_odonts[121,,],
-                             final_mirrored_odonts[c(69:82),,],
-                             final_mirrored_odonts[122:123,,],final_mirrored_odonts[c(83:119),,],
-                             final_mirrored_odonts[c(124:2028),,],
-                             along= 1)
-                             
-
-
-#call it the final dataset again
-final_mirrored_odonts <- arranged_odonts
-
-#REMOVE THE EXTRA UNDESCRIBED SPECIMENS IF REQUIRED
-#Save the undescribed specimens first 
-final_mirrored_odonts_undescribed <- final_mirrored_odonts
-
-save(final_mirrored_odonts_undescribed, file = 'final mirrored odonts undescribed2.R')
+LM3_midline=colSums(LM3_bilat)/2
+LM4_midline=colSums(LM4_bilat)/2
 
 
 #Check
-View(dimnames(final_mirrored_odonts)[[3]])
-#Remove the undescribed 
-final_mirrored_odonts <- final_mirrored_odonts[,,-c(84, 85, 86, 137)]     
-#check 
-View(dimnames(final_mirrored_odonts)[[3]])
 
-save(final_mirrored_kogiids, file = 'final mirrored kogiids.R')
+#Then just visually check that you're happy the code worked (IMPORTANT!):
+## read in a specimen ply and plot these landmarks on it:
+Pipa=ply2mesh(file="E:/Smithsonian Postdoc/Year 1/Mandible scans/LMs and curves/disarticulated/ply/Coronodon havensteini.ply")
+shade3d(Pipa, col="white")
+spheres3d(LM1_midline[,1]) #bracketed is specimen number
+spheres3d(LM2_midline[,2])
+spheres3d(LM3_midline[,3]) #bracketed is specimen number
+spheres3d(LM4_midline[,4])
+spheres3d(ptsarray[c(9,33),, 4], col = 'green') #check how these look with 5, 69, 6, 70
+spheres3d(ptsarray[c(10,34),,4], col = 'red')
+spheres3d(ptsarray[c(12,35),, 4], col = 'blue') #check how these look with 5, 69, 6, 70
+spheres3d(ptsarray[c(13,36),,4], col = 'yellow')
 
-#Bind the 3 datasets 
-final_dataset=abind::abind(final_mirrored_odonts, final_mirrored_mysts, final_mirrored_archs, along = 3)
+#spheres3d(slidedlmsARCHS[c(58,112),,1], col = 'red')
+#Then you add these two new midline landmarks to your landmark set:
+Shape_data_with_bilats=abind::abind(arraylm, LM1_midline, LM2_midline, LM3_midline, LM4_midline, along=1)
 
-#What order is this in? 
-View(dimnames(final_dataset)[[3]])
+#Check the midline LMs
+spheres3d(Shape_data_with_bilats[c(1:36),,3], col = 'green', radius = 4)
+spheres3d(Shape_data_with_bilats[c(37:40),,3], col = 'red', radius = 4)
 
-#change the order to alphabetical for all 
-final_dataset=final_dataset[,,sort(dimnames(final_dataset)[[3]])]
-#check 
-View(dimnames(final_dataset)[[3]])
-save(final_dataset, file = 'final data set.R')
+
+#Delete superfluous LMs
+arraylm_midline <- Shape_data_with_bilats[-c(9,10,12,13,33,34,35,36),,]
+
+#Check 
+text3d(arraylm_midline[c(1:32),,3], text = 1:32)
+
+
+
+
+#Now rearrange so it's the same order as 
+
+#You can use abind to bind datasets - this rearranges the data into the same order as the mysts (i.e. without the weird 1-120, 2 = 121, 19 - 122, 20 -123)
+arranged_ptsarray=abind::abind(arraylm_midline[c(1:8),,], 
+                             arraylm_midline[29:30,,],
+                             arraylm_midline[11,,],
+                             arraylm_midline[31:32,,],
+                             arraylm_midline[c(14:32),,],
+                             along= 1)
+
+#Check 
+text3d(arranged_ptsarray[c(1:32),,3], text = 1:32)
+
